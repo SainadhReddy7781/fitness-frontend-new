3a849e0ba5cdbedb2257e5e22b9c48dec8d318f3
SainadhReddy7781 <vemulapati.sainadh@gmail.com>
2025-11-28 02:01:31 +0530

    Update login API endpoint URL

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -13,7 +13,7 @@ const Login = ({ setIsLoggedIn }) => {
   const handleLogin = async (e) => {
     e.preventDefault();
     try {
-      const res = await axios.post('http://localhost:9090/api/users/login', {
+      const res = await axios.post('http://localhost:30083/api/users/login', {
         email,
         password
       });
