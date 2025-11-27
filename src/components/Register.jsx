faf841a2a8da8713898f966b488d8077b90d526e
SainadhReddy7781 <vemulapati.sainadh@gmail.com>
2025-11-28 02:01:55 +0530

    Update registration API endpoint URL

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -11,7 +11,7 @@ const Register = () => {
   const handleRegister = async (e) => {
     e.preventDefault();
     try {
-      const res = await axios.post('http://localhost:9090/api/users/register', {
+      const res = await axios.post('http://localhost:30083/api/users/register', {
         email,
         password
       });
